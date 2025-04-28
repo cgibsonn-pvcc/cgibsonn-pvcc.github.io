897e0ce33e9e054b664d58b7f15bc937bb0f5887
cgibsonn-pvcc <cg37772@email.vccs.edu>
2025-04-28 16:09:01 -0400

    Update Snake.py

--- Snake.py
+++ Snake.py
@@ -3,15 +3,27 @@ XLIMIT, YLIMIT = 650, 650
 WIN = pygame.display.set_mode((XLIMIT, YLIMIT))
 pygame.display.set_caption("CyberSnake")
 
-def SnakeGameplay():
+CYAN = (100, 255, 255)
+
+FRAMESPERSECOND = 60
+
+def drawings_on_window():
+    WIN.fill(CYAN)
+    pygame.display.update()
+    
+
+def main():
+    clock = pygame.time.Clock()
     run = True
     while run:
+        clock.tick(FRAMESPERSECOND)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        WIN.fill((100, 255, 255))
+
+        drawings_on_window()
         
     pygame.quit()
     
-if __name__ == "__SnakeGameplay__":
-    SnakeGameplay()
+if __name__ == "__main__":
+    main()
